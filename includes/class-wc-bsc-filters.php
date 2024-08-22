e24a398642ea6f52bb33a31c0abc7ec7eb2ccd00
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-08-21 21:35:36 -0500

    Create class-wc-bsc-filters.php

---
+++ includes/class-wc-bsc-filters.php
@@ -0,0 +1,21 @@
+<?php
+class WC_BSC_Filters {
+    
+    public static function wpse_rest_batch_items_limit($limit)
+    {
+        $limit = 1000;
+
+        return $limit;
+    }
+
+    public static  function update_limit_for_products($limit, $products)
+    {
+        $limit = 1000;
+
+        return $limit;
+    }
+}
+
+
+
+
