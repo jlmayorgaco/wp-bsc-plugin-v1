35655878a0d4285dbf7094b8bdf53714ae2dc99d
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-08-23 13:34:02 -0500

    Update bsc_filter_slider.js

--- widgets/js/bsc_filter_slider.js
+++ widgets/js/bsc_filter_slider.js
@@ -84,11 +84,19 @@ const fromSlider = document.querySelector("#fromSlider");
 const toSlider = document.querySelector("#toSlider");
 const fromInput = document.querySelector("#fromInput");
 const toInput = document.querySelector("#toInput");
-fillSlider(fromSlider, toSlider, colorGray, colorPink, toSlider);
-setToggleAccessible(toSlider);
+
+
 
 fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
 toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
 fromInput.oninput = () =>
   controlFromInput(fromSlider, fromInput, toInput, toSlider);
 toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
+
+
+fillSlider(fromSlider, toSlider, colorGray, colorPink, toSlider);
+setToggleAccessible(toSlider);
+
+setTimeout(() => {
+  filtersBSC.setFilterPrice({ min: 0, max: 1000000 });
+}, 1000)
