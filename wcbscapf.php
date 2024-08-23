aa8d45938748cfac551db3d04e1038446418f61c
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-08-23 00:10:48 -0500

    Update wcbscapf.php

--- wcbscapf.php
+++ wcbscapf.php
@@ -72,7 +72,8 @@ class WC_BSC_Plugin {
 	}
 
 	private function init_widgets(){
-		include_once WC_BSC_PLUGIN_PATH . 'widgets/_widgets.php';
+		//include_once WC_BSC_PLUGIN_PATH . 'widgets/_widgets.php';
+		include_once WC_BSC_PLUGIN_PATH . 'widgets/widget-bsc-wc-shop-product-filters/_index.php';
 	}
 
     private function init_hooks() {
