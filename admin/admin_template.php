df50354449617e5f28135a1e50837f949ffd2afe
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-10-19 20:43:03 -0500

    Testing cI cd deploy.sh

--- admin/admin_template.php
+++ admin/admin_template.php
@@ -49,7 +49,7 @@ function do_render_admin_template()
         <section class="bsc__section section__categories">
             <article class="categories__reset">
                 <header>
-                    <h1> CATEGORIAS v.1.0.5 Final deplot.sh CI CD Testing </h1>
+                    <h1> CATEGORIAS v.1.0.6 Final deplot.sh CI CD Testing </h1>
                     <hr>
                 </header>
                 <content>
