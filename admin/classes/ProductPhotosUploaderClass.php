b190069e20056e29c3dc36646fed3cc523ad903e
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-11-18 09:13:45 -0500

    Remove spaces

--- admin/classes/ProductPhotosUploaderClass.php
+++ admin/classes/ProductPhotosUploaderClass.php
@@ -226,7 +226,7 @@ class ProductPhotosUploaderClass
 
         $subfolders = scandir($directory);
         $processedCount = 0;
-        $maxIterations = 50;
+        $maxIterations = 100;
 
         echo '<br> .... Starting processFoldersByPrefix<br>';
         echo '<br> .... <br>';
