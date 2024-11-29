3509dcc636ce27b91d56516753871d12b0f6e8df
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-11-28 22:20:51 -0500

    Update

--- includes/class-wc-bsc-scripts.php
+++ includes/class-wc-bsc-scripts.php
@@ -6,11 +6,11 @@ class WC_BSC_Scripts {
         echo '<br>';
         echo '<br>';
         echo ' ... wp_enqueue  x2 ...';
-        echo  WC_BSC_PLUGIN_URL . 'assets/js/bsc_wc_shop_product_filters.js';
+        echo  WC_BSC_PLUGIN_URL . 'widgets/js/bsc_wc_shop_product_filters.js';
         echo '<br>';
         echo '<br>';
         echo '<br>';
-        wp_enqueue_script('bsc_wc_shop_product_filters_script', WC_BSC_PLUGIN_URL . 'assets/js/bsc_wc_shop_product_filters.js', ['jquery'], WC_BSC_PLUGIN_VERSION, true);
+        wp_enqueue_script('bsc_wc_shop_product_filters_script', WC_BSC_PLUGIN_URL . 'widgets/js/bsc_wc_shop_product_filters.js', ['jquery'], WC_BSC_PLUGIN_VERSION, true);
         wp_localize_script('bsc_wc_shop_product_filters_script', 'ajax_var', [
             'url'    => admin_url('admin-ajax.php'),
             'nonce'  => wp_create_nonce('my-ajax-nonce'),
