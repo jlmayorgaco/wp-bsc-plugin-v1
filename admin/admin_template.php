876c8df17fec09e481f819c055a2449eb263d4cb
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-11-30 00:55:13 -0500

    Update

--- admin/admin_template.php
+++ admin/admin_template.php
@@ -167,7 +167,9 @@ function do_render_admin_template()
                     <?php
                         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['process_photos_products'])) {
                             try {
-                                BSC_PPU_Init::init();
+                                $command = "wp eval 'BSC_PPU_Init::init();' > /dev/null 2>&1 &";
+                                exec($command);
+                                echo "BSC_PPU_Init is running in the background.";
                                 echo '<p>Photos processed successfully.</p>';
                             } catch (Exception $e) {
                                 echo '<p style="color:red;">Error: ' . $e->getMessage() . '</p>';
