fc00f13d70f9fd0a0074b9a77e730ceefc2c7343
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-12-02 15:01:29 -0500

    Update

--- admin/admin_template.php
+++ admin/admin_template.php
@@ -60,7 +60,7 @@ function do_render_admin_template()
         <section class="bsc__section section__categories">
             <article class="categories__reset">
                 <header>
-                    <h1> CATEGORIAS v.1.0.3 CI CD WebHook </h1>
+                    <h1> CATEGORIAS v.1.0.4 CI CD WebHook </h1>
                     <hr>
                 </header>
                 <content>
