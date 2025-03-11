3b56a198a3dfb33ba38e3064cb63a25e3439cf76
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-03-11 16:27:45 -0500

    Create _index.php

---
+++ widgets/widget-bsc-wc-shop-products-slider/_index.php
@@ -0,0 +1,12 @@
+<?php
+
+    if (!defined('ABSPATH')) {
+        exit; // Exit if accessed directly
+    }
+
+    // Import Rendere Services
+    include_once WC_BSC_PLUGIN_PATH . 'widgets/widget-bsc-wc-shop-products-slider/_shortcode_fixed.php';
+    include_once WC_BSC_PLUGIN_PATH . 'widgets/widget-bsc-wc-shop-products-slider/_shortcode_slider.php';
+    include_once WC_BSC_PLUGIN_PATH . 'widgets/widget-bsc-wc-shop-products-slider/_shortcode_tabs.php';
+    
+?>
